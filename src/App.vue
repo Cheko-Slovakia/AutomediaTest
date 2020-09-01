8a6cc49f0d688125cf09cc6e766cd356c2026319
Oliver <oliver.dr98@gmail.com>
2020-09-01 12:51:10 +0200

    Added the solution

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NumberInput
+    v-for="NumberInputs in NumberInputArray"
+    v-bind:key="NumberInputs.id"
+    v-bind:val="NumberInputs.val"
+    v-on:update-value="setValNumberInput"
+    ></NumberInput>
+
+    <div>
+      <button v-on:click="addNumberInput" >
+      {{total}}
+      </button>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumberInput from './components/NumberInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NumberInput
+  },
+  data: function() {
+    return {
+     NumberInputArray: [
+       {id: 1, val: 10 },
+       {id: 2, val: 20},
+       {id: 3, val: 25},
+       {id: 4, val: 25},
+       {id: 5, val: 20}
+     ]
+    };
+  },
+  computed: {
+    total: function (){
+      var sum = 0
+      for(var i = 0; i<this.NumberInputArray.length; i++){
+        sum += this.NumberInputArray[i].val
+      }
+      return sum
+    }
+  },
+  methods: {
+    setValNumberInput: function (id, newVal){
+      if(newVal != "" ){
+        this.NumberInputArray[id-1].val = parseInt(newVal)
+      } else {
+        this.NumberInputArray[id-1].val = 0;
+      }
+    },
+    addNumberInput: function (){
+      console.log('new: %s', this.NumberInputArray.length-1)
+      this.NumberInputArray.push({id: this.NumberInputArray.length+1,
+                                  val: this.getRandomNumber()})
+    },
+    getRandomNumber: function (){
+      return Math.floor(Math.random()*(10-1+1)+1)
+    }
   }
 }
 </script>
+
 
 <style>
 #app {
@@ -24,5 +73,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+ h3 {
+   margin: 40px 0 0;
+ }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
---
+++ src/components/NumberInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="number-input">
+    <input v-model="updatedVal" type="number" v-on:input="$emit('update-value', theKey, $event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberInput',
+  props: {
+    val: Number
+  },
+  computed :{
+    updatedVal: {
+      get() {
+        return this.val
+      },
+      set(input) {
+        return input
+      }
+    }
+  },
+  data: function () {
+    return {
+      theKey: this.$vnode.key
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
